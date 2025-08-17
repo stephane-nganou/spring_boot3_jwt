b4941968a775f458cbccf7b5ecdcc5509a4bd0c2
StephaneWafo <stephanekechi@gmail.com>
2025-08-18 00:06:24 +0200

    refactoring PUBLIC_ENDPOINTS

--- src/main/java/com/masteranything/security/config/security/SecurityConfiguration.java
+++ src/main/java/com/masteranything/security/config/security/SecurityConfiguration.java
@@ -23,6 +23,19 @@ import lombok.AllArgsConstructor;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
 
+  private static final String[] PUBLIC_ENDPOINTS = {
+        "/api/v1/auth/**",
+        "/v2/api-docs",
+        "/v3/api-docs",
+        "/swagger-resources",
+        "/swagger-resources/**",
+        "/configuration/ui",
+        "/configuration/security",
+        "/swagger-ui/**",
+        "/webjars/**",
+        "/swagger-ui.html"
+    };
+
   private final JwtAuthenticationFilter jwtAuthFilter;
   private final AuthenticationProvider authenticationProvider;
 
@@ -41,19 +54,10 @@ public class SecurityConfiguration {
         .cors(Customizer.withDefaults())
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth ->
-            auth.requestMatchers(
-                "/api/v1/auth/**",
-                "/v2/api-docs",
-                "/v3/api-docs",
-                "/swagger-resources",
-                "/swagger-resources/**",
-                "/configuration/ui",
-                "/configuration/security",
-                "/swagger-ui/**",
-                "/webjars/**",
-                "/swagger-ui.html"
-            ).permitAll()
-            .anyRequest().authenticated()
+            auth.requestMatchers(PUBLIC_ENDPOINTS)
+            .permitAll()
+            .anyRequest()
+            .authenticated()
         )
         .sessionManagement(session -> session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
